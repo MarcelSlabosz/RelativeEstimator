f569972dff031993a6745dc76cb8346e06a20a19
Marcel Słabosz <slabosz.marcel@gmail.com>
2024-03-28 22:58:19 +0100

    Creating an output folder if it does not exist during the generation of the output file.

--- server/relative_estimator/output_writer.py
+++ server/relative_estimator/output_writer.py
@@ -10,13 +10,20 @@ class JiraREOutputException(JiraRelativrEstimatorException):
 
 class JSONWriter:
     def __init__(self, configuration):
+        self._file = None
         self.__config = configuration
-
         self.__logger = logging.getLogger(__name__)
         self.__explode_config()
+        self.__create_directory_if_not_exists(self._file)
 
     def __explode_config(self):
         self._file = self.__config['file']
+
+    def __create_directory_if_not_exists(self, file_path):
+        import os
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
     def write_to_file(self, issues):
         file_content = {"items": issues}
